c242857567e834f7aa1c4370f444966a0ace8367
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2023-08-14 20:34:38 +0100

    Updating runtime version of AWS Lambda.

--- nextjs-lambda-cdk/lib/nextjs-lambda-cdk-stack.ts
+++ nextjs-lambda-cdk/lib/nextjs-lambda-cdk-stack.ts
@@ -20,7 +20,7 @@ export class NextjsLambdaCdkStack extends Stack {
     );
 
     const nextCdkFunction = new lambda.Function(this, 'NextCdkFunction', {
-      runtime: lambda.Runtime.NODEJS_16_X,
+      runtime: lambda.Runtime.NODEJS_18_X,
       handler: 'run.sh',
       code: lambda.Code.fromAsset(
         path.join(__dirname, '../app/.next/', 'standalone')
